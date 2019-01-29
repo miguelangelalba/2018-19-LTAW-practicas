7edb9e403f9d2c73cda4db71566fec3a20bc5ecc
Miguel Ngel Alba Blanco <maalbab@beta07.aulas.gsyc.urjc.es>
2019-01-29 12:42:35 +0100

    Añado hora al servidor

--- Practica-1/web1/server.js
+++ Practica-1/web1/server.js
@@ -1,10 +1,12 @@
 var http = require('http');
+var dt = require('./myfirstmodule');
 
 console.log("Arrancando servidor...")
 
 
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/html'});
+  res.write("The date and time are currently: " + dt.myDateTime());
   res.end('Hello World!');
   console.log("Peticion atendida")
 }).listen(8080);
