289a3b393ea8aa4ecf60374f448bf77df26f37ea
Miguel Ángel Alba <miguelangel.abb@gmail.com>
2019-02-10 15:07:48 +0100

    Primera versión de server con css. En este ejemplo he utilizado mi vc del año pasado para ver el funcionamiento de la página. Lo srive bien. En las siguientes versiones iré modificando esta web para hacer una tienda de xiaomi.

--- Practica-1/web1/server.js
+++ Practica-1/web1/server.js
@@ -2,19 +2,30 @@ var http = require('http');
 var dt = require('./myfirstmodule');
 var url = require('url');
 var fs = require('fs');
+const mime = {
+   'html' : 'text/html',
+   'css'  : 'text/css',
+   'jpg'  : 'image/jpg',
+   'ico'  : 'image/x-icon',
+   'mp3'  :	'audio/mpeg3',
+   'mp4'  : 'video/mp4'
+};
 
 console.log("Arrancando servidor...");
 
 http.createServer(function (req, res) {
-    var q = url.parse(req.url, true);
-    console.log(q.pathname);
-    var filename = "." + q.pathname;
+    var objetourl = url.parse(req.url, true);
+    console.log(objetourl.pathname);
+    var filename = "." + objetourl.pathname;
     fs.readFile(filename, function(err, data) {
     if (err) {
       res.writeHead(404, {'Content-Type': 'text/html'});
       return res.end("404 Not Found");
     }
-    res.writeHead(200, {'Content-Type': 'text/html'});
+    const vec = filename.split('.');
+    const extension=vec[vec.length-1];
+    const mimearchivo = mime[extension];
+    res.writeHead(200, {'Content-Type': 'mimearchivo'});
     res.write(data);
     return res.end();
     console.log("Peticion atendida")
