656d13e442bdaac7c35bca0ad98067670f927762
Miguel Ángel Alba <miguelangel.abb@gmail.com>
2019-04-18 13:20:05 +0200

    Añado comentarios para aclarar código

--- Practica-3/web1/busca.js
+++ Practica-3/web1/busca.js
@@ -1,14 +1,23 @@
 function main(){
+    //Cojemos las variables del DOm que sean necesarias
     var ver = document.getElementById('Buscar');
     var resultado = document.getElementById('resultado');
     ver.onclick = ()=>{
+        //Crear objeto para la pretición Ajax
         m = new XMLHttpRequest();
+        //Configuración de la Petición
         m.open("GET","http://localhost:8080/myquery", true);
+
+        //Qeuda a la espera para ejecutar esta función (es una promesa?)
         m.onreadystatechange=function(){
+            //Si la petición ha sido enviada y recibida....
             if (m.readyState==4 && m.status==200){
+
                 var o = JSON.parse(m.responseText)
                 resultado.innerHTML = "";
                 for (i=0; i < o.productos.length; i++) {
+                    //REcuerda, productos es un objeto que contiene un aray, definido
+                    //En el servidor
                     resultado.innerHTML += o.productos[i];
                     if (i<o.productos.length-1) {
                         resultado.innerHTML += ', ';
