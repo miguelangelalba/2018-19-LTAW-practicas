b7f6b02376a9e5354b511dd7c27cb34393fea3c2
Miguel Ángel Alba <miguelangel.abb@gmail.com>
2019-04-21 22:13:25 +0200

    Anotaciones

--- Practica-3/web1/formularioCompra.js
+++ Practica-3/web1/formularioCompra.js
@@ -1,6 +1,7 @@
 function cerrarSesion(){
 
     location.href="http://localhost:8080/index.html";
+    //Debo preguntar, supuestamente con el expires debería de caducar la cookie y no mandarla
     document.cookie = "expires=Thu, 01 Jan 1970 00:00:01 GMT";
 
 }
